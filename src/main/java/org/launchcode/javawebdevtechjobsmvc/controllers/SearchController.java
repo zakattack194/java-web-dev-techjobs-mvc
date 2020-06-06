4264c06a2c05afc530c6f5f323cb6ffe6e6344b7
Zachary A Kniffen <zacharyakniffen@gmail.com>
2020-06-05 22:54:35 -0500

    Passed all Sanity Checks.  Fingers Crossed.

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -25,4 +25,19 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
+    @RequestMapping(value = "/results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        ArrayList<Job> jobs;
+        if (searchTerm.toLowerCase().equals("all") || searchTerm.equals("")){
+            jobs = JobData.findAll();
+            model.addAttribute("title", "All Jobs");
+            model.addAttribute("columns", columnChoices);
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+            model.addAttribute("columns", columnChoices);
+        }
+        model.addAttribute("jobs", jobs);
+        return "search";
+    }
 }
